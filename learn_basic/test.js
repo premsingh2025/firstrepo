d7e6cc9204bdb5f09ea27bfcbec44072e6676383
Prem Singh <officialpremsingh03@gmail.com>
2025-06-16 03:17:06 +0530

    completed

--- learn_basic/test.js
+++ learn_basic/test.js
@@ -1,4 +1,13 @@
 const accountId = 14466
 let accountEmail = "singhprem@gmail.com"
 var accountPassword = "12345"
-accountCity = "jaipur"
+var accountCity = "jaipur"
+
+
+//accountId = 2 not allowed
+accountEmail = "premsinhgh.vom"
+accountCity = "benglore"
+
+console.log(accountId);
+
+console.table([accountId,accountCity,accountEmail,])
