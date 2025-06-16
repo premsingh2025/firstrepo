d8b4869c17f5c3ca116e0a6bb5637640cdceb7a5
Prem Singh <officialpremsingh03@gmail.com>
2025-06-17 01:58:17 +0530

    conversion of datatype

---
+++ learn_basic/test2.js
@@ -0,0 +1,22 @@
+
+let score = "prem"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//let isLogedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isLogedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 = true , 0=>false 
+// "" => empty thats means false 
+//"prem" this means true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
